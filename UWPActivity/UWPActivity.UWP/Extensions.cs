f16028e7417be6281c87d02eb4347bf8c226a9ab
Ninaada Bellippady <ninaada@users.noreply.github.com>
2021-09-15 00:04:09 +0530

    initial commit

---
+++ UWPActivity/UWPActivity.UWP/Extensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
+
+namespace UWPActivity.UWP
+{
+    public static class Extensions
+    {
+        public static Color ToUwPColor(this Xamarin.Forms.Color color)
+        {
+            return Color.FromArgb(
+                Convert.ToByte(color.A * 255),
+                Convert.ToByte(color.R * 255),
+                Convert.ToByte(color.G * 255),
+                Convert.ToByte(color.B * 255));
+        }
+
+        public static SolidColorBrush ToUwpSolidColorBrush(this Xamarin.Forms.Color color)
+        {
+            return new SolidColorBrush(color.ToUwPColor());
+        }
+    }
+}
---
+++ UWPActivity/UWPActivity.UWP/UWPIndicatorRenderer.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using System;
+using System.ComponentModel;
+using UWPActivity;
+using UWPActivity.UWP;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.UWP;
+
+[assembly: ExportRenderer(typeof(UWPIndicator), typeof(UWPIndicatorRenderer))]
+namespace UWPActivity.UWP
+{
+    public class UWPIndicatorRenderer : ViewRenderer<UWPIndicator, ProgressRing>
+    {
+        ProgressRing _progressRing;
+        protected override void OnElementChanged(ElementChangedEventArgs<UWPIndicator> e)
+        {
+            try
+            {
+                base.OnElementChanged(e);
+
+                if (Control != null)
+                    return;
+
+                _progressRing = new ProgressRing();
+
+                if (e.NewElement != null)
+                {
+                    _progressRing.IsActive = Element.IsRunning;
+                    _progressRing.Visibility = Element.IsRunning ? Windows.UI.Xaml.Visibility.Visible : Windows.UI.Xaml.Visibility.Collapsed;
+                    var xfColor = Element.Color;
+                    _progressRing.Foreground = xfColor.ToUwpSolidColorBrush();
+                    SetNativeControl(_progressRing);
+                }
+
+                //if (Control == null)
+                //{
+                //    _progressRing = new ProgressRing();
+                //    _progressRing.IsActive = true;
+                //    _progressRing.Visibility = Visibility.Visible;
+                //    _progressRing.IsEnabled = true;
+                //    SetNativeControl(_progressRing);
+                //}
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            try
+            {
+                base.OnElementPropertyChanged(sender, e);
+
+                if (e.PropertyName == nameof(ActivityIndicator.Color))
+                {
+                    _progressRing.Foreground = Element.Color.ToUwpSolidColorBrush();
+                }
+                if (e.PropertyName == nameof(ActivityIndicator.IsRunning))
+                {
+                    _progressRing.IsActive = Element.IsRunning;
+                    _progressRing.Visibility = Element.IsRunning ? Windows.UI.Xaml.Visibility.Visible : Windows.UI.Xaml.Visibility.Collapsed;
+                }
+                if (e.PropertyName == nameof(ActivityIndicator.WidthRequest))
+                {
+                    _progressRing.Width = Element.WidthRequest > 0 ? Element.WidthRequest : 20;
+                    UpdateNativeControl();
+                }
+                if (e.PropertyName == nameof(ActivityIndicator.HeightRequest))
+                {
+                    _progressRing.Height = Element.HeightRequest > 0 ? Element.HeightRequest : 20;
+                    UpdateNativeControl();
+                }
+                if (_progressRing.Height != _progressRing.Width)
+                {
+                    double min = Math.Min(Element.HeightRequest, Element.WidthRequest);
+                    if (min <= 0)
+                    {
+                        min = Math.Max(Element.HeightRequest, Element.WidthRequest);
+                    }
+                    if (min > 0)
+                    {
+                        _progressRing.Height = _progressRing.Width = min;
+                        _progressRing.MaxHeight = _progressRing.MaxWidth = min;
+                        UpdateNativeControl();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
